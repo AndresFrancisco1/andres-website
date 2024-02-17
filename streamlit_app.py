4b4b89386505fdc46074de6084d6ff8298515eaf
AndresFrancisco1 <146501795+AndresFrancisco1@users.noreply.github.com>
2024-02-17 10:30:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ def get_base64(bin_file):
     return base64.b64encode(data).decode()
 
 def set_background(png_file):
-    bin_str = get_base64(png_file)
+    bin_str = get_base64(gif_file)
     page_bg_img = '''
     <style>
     .main {
@@ -36,7 +36,7 @@ def set_background(png_file):
     ''' % bin_str
     st.markdown(page_bg_img, unsafe_allow_html=True)
     
-set_background("Images/test.gif")
+set_background("Images/Test.gif")
 
 # Use local CSS
 def local_css(file_name):
@@ -49,7 +49,7 @@ local_css("style/style.css")
 lottie_coding = load_lottieurl("https://lottie.host/9c93ef77-0238-419f-9df2-ba3d2371f360/IU6MBRUGKX.json")
 img_cs16 = Image.open("Images/mqdefault.webp")
 img_andres = Image.open("Images/andres1.jpg")
-png_file = Image.open("Images/test.gif")
+gif_file = Image.open("Images/Test.gif")
 
 # ---- MY PROFILE ----
 st.subheader("Hi, I am Andres Luis Francisco, but u can call me Andres or Luis :wave:")
