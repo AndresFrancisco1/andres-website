920f3297a3385425643ad1acbb4bc38cb2e6e425
AndresFrancisco1 <146501795+AndresFrancisco1@users.noreply.github.com>
2024-02-24 08:02:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,17 @@ def get_base64(bin_file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-
+def set_background(gif_file):
+    bin_str = get_base64(gif_file)
+    page_bg_img = '''
+    <style>
+    .main {
+    background-image: url("data:image/hif;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
     
 set_background("Images/Test.gif")
 
